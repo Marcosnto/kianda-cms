09bb413ad8fdbd126a511d74ebc2ac777dc2acd6
Marcos Neto <Marcos_nto@hotmail.com>
2024-04-24 22:49:02 -0300

    Initial commit

---
+++ src/pages/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+export function sum(a: number, b: number) {
+  return a + b;
+}
+
+function App() {
+  return (
+    <>
+      <div>Hello world!</div>
+    </>
+  );
+}
+
+export default App;
