f5a6a1275f5c24c8ea008133e1156acc27fca47e
Marcos Neto <marcos_nto@hotmail.com>
2024-04-28 12:30:07 -0300

    build: configure aliases

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,16 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es6: true, es2021: true, node: true },
+  settings: {
+    react: {
+      version: "detect",
+    },
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
   extends: [
     "eslint:recommended",
     "plugin:import/errors",
@@ -15,6 +25,14 @@ module.exports = {
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: ["./tsconfig.json", "./tsconfig.node.json"],
+  },
   plugins: [
     "react-refresh",
     "react",
@@ -23,6 +41,7 @@ module.exports = {
     "jsx-a11y",
     "@typescript-eslint",
     "react-hooks",
+    "'import-resolver-alias",
   ],
   rules: {
     "react-refresh/only-export-components": [
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 /// <reference types="vitest" />
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@components": path.resolve(__dirname, "./src/components"),
+    },
+  },
   plugins: [react()],
   test: {
     globals: true,
