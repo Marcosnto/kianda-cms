fc7925849e7848c954940a4b1b801a94e7021911
Marcos Neto <marcos_nto@hotmail.com>
2024-05-19 22:58:02 -0300

    fix: remove head from nextjs

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,5 +1,4 @@
 import { Box, Center } from "@chakra-ui/react";
-import Head from "next/head";
 
 import UserAutoRegister from "../components/Forms/User/AutoRegister";
 import LogoImage from "../components/LogoImage";
@@ -7,9 +6,8 @@ import LogoImage from "../components/LogoImage";
 export default function Register() {
   return (
     <Box background="green.900">
-      <Head>
-        <title>Kianda - Registrar-se</title>
-      </Head>
+      <title>Kianda - Registrar-se</title>
+
       <Center flexDirection="column" pt="15" pb="15" gap="5">
         <LogoImage pathRedirect="/login" />
         <UserAutoRegister />
