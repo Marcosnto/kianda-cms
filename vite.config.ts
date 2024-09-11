9dcd2191a193c51ead16a7ca2d46799d93099330
Marcos Neto <marcos_nto@hotmail.com>
2024-09-11 00:26:29 -0300

    build: modify envoriment variables name

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,30 @@
 /// <reference types="vitest" />
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  define: {
-    "process.env.NODE_ENV": JSON.stringify(
-      process.env.NODE_ENV || "development",
-    ),
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@assets": path.resolve(__dirname, "./src/assets"),
-      "@components": path.resolve(__dirname, "./src/components"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    define: {
+      "process.env.NODE_ENV": JSON.stringify(env.NODE_ENV || "development"),
     },
-  },
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./setupTests.ts",
-    css: true,
-    reporters: ["verbose"],
-  },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+        "@assets": path.resolve(__dirname, "./src/assets"),
+        "@components": path.resolve(__dirname, "./src/components"),
+      },
+    },
+    plugins: [react()],
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "./setupTests.ts",
+      css: true,
+      reporters: ["verbose"],
+    },
+  };
 });
